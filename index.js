3d03d12a793d535854f675f9ce2b7946cd36b263
Alan C <alanc@kidney.org>
2018-01-25 09:37:57 -0500

    Made Index in Public HTML Directory default static file to serve

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.set('view engine', 'pug');
 
 app.use('/', express.static('public'));
 
-app.get('/',(req,res) => res.send('index'))
+app.get('/',(req,res) => res.sendFile('index.html'))
 
 app.listen(3000, () => console.log('This is working!'))
